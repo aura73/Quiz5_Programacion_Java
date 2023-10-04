606250ef34753f7198a2f18c9fe1afb42097610d
aura73 <142046943+aura73@users.noreply.github.com>
2023-10-04 17:02:46 -0500

    Update Principal.java
    Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,9 @@
 public class Principal {
     public static void main(String[] args) {
+    // Crear una instancia de la clase Biblioteca llamada bibliotecaKonradLorenz.
        Biblioteca bibliotecaKonradLorenz = new Biblioteca();
 
+        // Crear varios objetos de la clase Libro.
        Libro libro1 = new Libro("Cien años de soledad","Gabriel García Márquez", 496);
        Libro libro2 = new Libro("El código Da Vinci", "“Dan Brown", 656);
        Libro libro3 = new Libro("1984", "George Orwell", 326);
